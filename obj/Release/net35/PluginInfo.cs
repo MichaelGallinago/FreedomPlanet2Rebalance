178179917c47b619c1bbd40f203f93e7b0f88968
MichaelGallinago <67271938+MichaelGallinago@users.noreply.github.com>
2022-10-22 03:44:15 +0500

    BeplnEX port & config update

---
+++ obj/Release/net35/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace FP2Rebalance
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "FP2Rebalance";
+        public const string PLUGIN_NAME = "FP2Rebalance";
+        public const string PLUGIN_VERSION = "1.6.3";
+    }
+}
