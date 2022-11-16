95f5c066b0df4258ae68c24ce4d74d7edfed968e
Michael Gallinago <michael.gallinago@gmail.com>
2022-11-17 00:00:41 +0500

    No more code 8P

--- obj/Release/net35/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace FP2Rebalance
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "FP2Rebalance";
-        public const string PLUGIN_NAME = "FP2Rebalance";
-        public const string PLUGIN_VERSION = "1.6.3";
-    }
-}
